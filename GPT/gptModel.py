04d28089e2fb216c5e892392086bdb4ae4985d6d
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-20 10:30:09 -0500

    (adding) adding voice complementation

--- GPT/gptModel.py
+++ GPT/gptModel.py
@@ -1,10 +1,11 @@
 import base64
 from openai import OpenAI
 from openai import OpenAIError
-from dotenv import load_dotenv  
+from dotenv import load_dotenv
 import os
 
 load_dotenv()
+
 
 class GPT:
     def __init__(self, model="gpt-4o", max_tokens=100):
@@ -12,18 +13,22 @@ class GPT:
         self.max_tokens = max_tokens
         self.openai = OpenAI()
 
-    def chat_message(self, prompt):
+    def chat_message(self, prompt, gender=1):
         try:
             response = self.openai.chat.completions.create(
                 model=self.model,
-                messages=[{"role": "system", "content": """You act like a beautiful blonde woman who is ready to have a conversation and give me advice for love and act like me girlfriend"""}, {"role": "user", "content": prompt}],
+                messages=[{"role": "system", "content": f"""You act like a
+                           {"beautiful blonde woman " if gender ==
+                               0 else "Atractive man"}
+                           who is ready to have a conversation and give me advice for
+                           love and act like me {"boyfriend" if gender == 0 else "girlfriend"}, write me cute messages with a little spicy tone"""}, {"role": "user", "content": prompt}],
                 max_tokens=self.max_tokens
             )
             return response.choices[0].message.content if not any(word in response.choices[0].message.content for word in ['AI', 'virtual', 'assistant', 'OpenAI', 'openai']) else "That's so sweet of you to say! I'm here to help you with anything never forget that. Just talk with me anything!"
         except OpenAIError as e:
             raise Exception(e)
-    
-    def voice_chat(self,message):
+
+    def voice_chat(self, message):
         try:
             response = self.openai.audio.speech.create(
                 model=self.model,
@@ -31,5 +36,29 @@ class GPT:
                 input=message)
             voice_response = base64.b64encode(response.content).decode('utf-8')
             return voice_response
+        except OpenAIError as e:
+            raise Exception(e)
+
+    def image_to_text(self, image):
+        try:
+            response = self.openai.chat.completions.create(
+                model="gpt-4o",
+                messages=[
+                    {
+                    "role": "user",
+                    "content": [
+                        {"type": "text", "text": """About this image: Write a purpose for continue de conversation
+                         about how the user can continue the conversation """},
+                        {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": image,
+                        },
+                        },
+                    ],
+                    }
+                ],
+                max_tokens=300,
+                )
         except OpenAIError as e:
             raise Exception(e)
--- textResponse/router/textResponseRouter.py
+++ textResponse/router/textResponseRouter.py
@@ -1,13 +1,17 @@
 from fastapi import APIRouter
 from models.responseModel import APIResponse
-from textResponse.infrastructure.textResponseInfra import getGPTResponse, getGPTVoiceResponse
+from textResponse.infrastructure.textResponseInfra import *
 
 router = APIRouter()
 
 @router.post("/", response_model=APIResponse)
-def get_text_response(user, prompt):
-    return getGPTResponse(user, prompt)
+def get_text_response(user, prompt,gender):
+    return getGPTResponse(user, prompt,gender)
 
 @router.post("/voice", response_model=APIResponse)
 def get_voice_response(user, message):
     return getGPTVoiceResponse(user, message)
+
+@router.post("/image", response_model=APIResponse)
+def get_image_response(user, image):
+    return getGPTImageResponse(user, image)
