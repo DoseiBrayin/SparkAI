c6db958d5daccdf7847d673398e4117521c8a397
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-22 00:34:51 -0500

    Change model id

--- onboarding/models/signUpModel.py
+++ onboarding/models/signUpModel.py
@@ -1,9 +1,11 @@
 from pydantic import BaseModel
 
 class SignUpModel(BaseModel):
+    id: str
     name: str
     email: str
     password: str
     age_range: str
     personality: str
     gender: str
+    payment: str = None
