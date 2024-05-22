2b0af311c609233b4ec60228cdbd11c8e114f242
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-22 00:34:41 -0500

    (Add) Adding endpoint signup

--- onboarding/router/onboardingRouter.py
+++ onboarding/router/onboardingRouter.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 from models.responseModel import APIResponse
-from onboarding.infrastructure.onboardingInfra import login
+from onboarding.infrastructure.onboardingInfra import login, signUp
 from onboarding.models.loginModel import LoginModel
+from onboarding.models.signUpModel import SignUpModel
 
 router = APIRouter()
 
@@ -9,3 +10,6 @@ router = APIRouter()
 def login_user(login_user: LoginModel, response_model=APIResponse):
     return login(login_user)
 
+@router.post("/signUp")
+def signUp_user(signUp_user: SignUpModel, response_model=APIResponse):
+    return signUp(signUp_user)
