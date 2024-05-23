efa35dd253e2b623ed59e72f3348d7973487b51e
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-05-23 09:33:37 -0500

    (fix) login try errors

--- onboarding/infrastructure/onboardingInfra.py
+++ onboarding/infrastructure/onboardingInfra.py
@@ -6,31 +6,29 @@ from db.models.sparkDbModel import User,Gender,Payment,Personality
 from fastapi import HTTPException
 import uuid
 
-
 def login(login: LoginModel):
     try:
         session = Session()
         user = session.query(User).filter(User.email == login.email, User.password == login.password).first()
-        if user:
-            return APIResponse(status="Success", message="Login Successfull", data=user.to_dict(),status_code=200)
-        else:
-            raise HTTPException(status_code=401, detail=APIResponse(status="Failed", message="Invalid Credentials", data=None,status_code=401).__dict__)
     except Exception as e:
         raise HTTPException(status_code=500, detail=APIResponse(status="Failed", message="Internal Server Error", data=str(e),status_code=500).__dict__)
     finally:
         session.close()
+        if user:
+            return APIResponse(status="Success", message="Login Successfull", data=user.to_dict(),status_code=200)
+        else:
+            raise HTTPException(status_code=401, detail=APIResponse(status="Failed", message="Invalid Credentials", data=None,status_code=401).__dict__)
 
 def signUp(signUp: SignUpModel):
     try:
         session = Session()
         user = session.query(User).filter(User.email == signUp.email).first()
+        gender = session.query(Gender).get(signUp.fk_gender)
+        payment = session.query(Payment).get(signUp.fk_payment)
+        personality = session.query(Personality).get(signUp.fk_personality)
         if user:
             raise HTTPException(status_code=401, detail=APIResponse(status="Failed", message="User Exists", data=None,status_code=401).__dict__)
         else:
-            gender = session.query(Gender).get(signUp.fk_gender)
-            payment = session.query(Payment).get(signUp.fk_payment)
-            personality = session.query(Personality).get(signUp.fk_personality)
-
             newUser = User(
                 id=str(uuid.uuid4()),
                 name=signUp.name,
