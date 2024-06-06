d71eafd531b7413a342f85a49aa2d36b16b71d77
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-06-06 11:20:08 -0500

    (config) Changing the messages context

--- helpers/getChatContext.py
+++ helpers/getChatContext.py
@@ -1,7 +1,7 @@
 from db.mongoConection import chats_collection
 
 def getChatContext(user:str) -> dict:
-    context = chats_collection.find_one({"user_id":user}, {"messages": {"$slice": -6}})
+    context = chats_collection.find_one({"user_id":user}, {"messages": {"$slice": -10}})
     if context and 'messages' in context:
         return [{"sender": msg["sender"], "message": msg["messages"]} for msg in context["messages"]]
     else:
